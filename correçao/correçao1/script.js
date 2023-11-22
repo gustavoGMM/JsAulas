e1a36d3eb4b8c01d1d1287658892150761397648
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-22 08:36:34 -0300

    update

--- correçao/correçao1/script.js
+++ correçao/correçao1/script.js
@@ -7,10 +7,11 @@
 //alert(`O valor pra cada cliente é de R$ ${valorDivido}`);
 
 function calculeConta(clientes , valorTotal){
-alert("atividade 1 - calcula conta");
 var valorDivido = valorTotal / clientes;
-alert(`O valor pra cada cleinte é de R$ ${valorDivido}`);
+alert(`O valor pra cada cleinte é de R$ ${parseInt(valorDivido.toFixed(2))}`);
+//parseInt(valorDivido*100)/100
 }
+alert("Atividade 1 - Calculadora conta");
 var clientes = Number(prompt("Quantos clientes estão na conta?"))
-var valorTotal = Number(prompt("qual é o valor total da conta?"))
+var valorTotal = Number(prompt("Qual é o valor total da conta?"))
 calculeConta(clientes,valorTotal)
---
+++ correçao/correçao1/script1.js
@@ -0,0 +1,4 @@
+var numero = Number(prompt ("qual é o numero desejado"));
+
+// var sucessor = numero + 1;
+// var 
