ed7307ca02235b0f989d987c3237bf38f364c53e
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-22 09:16:54 -0300

    update

---
+++ aula02 - condicional/js/Script.js
@@ -0,0 +1,30 @@
+//OPERADORES LOGICOS (COMPARAR)
+// > MAIOR QUE
+// < MENOR QUE
+// != DIFERENTE (<>)
+// == IGUAL (= RECEBE, == COMPARE VALOR, === COMPARA VALOR E TIPO )
+// >= MAIOR OU IGUAL
+// <= MENOR OU IGUAL
+
+// OPERADORES ALTERNÀRIOS
+// && E (DUAS CONDIÇÔES DEVERÂO SER IGUAIS EM RESULTADO)
+// || OU (SE UMA DAS CONDIÇÒES FOR VERDADEIRA SERÀ EXECUTADO)
+// ! NÂO (SE NÃO FOR A CONDIÇÃO)
+
+//CONDICIONAIS 
+// IF (SE SENAO)
+if(true){
+    console.log("Sou verdadeiro");
+}else{
+    console.log("Sou falso")
+}
+
+// verificar idade
+var idade = Number(prompt("Qual é a sua idade?"));
+
+if(idade >= 18){
+    alert("Seja bem vindo");
+}else{     
+     alert("Voce nao tem autorizaçao pra entrar no site");
+     location.href = "https://www.google.com.br/";
+}
