677d103e6a5420a6206ca954c7d0de3f18cc8a49
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-23 08:16:36 -0300

    update

--- aula02 - condicional/js/Script.js
+++ aula02 - condicional/js/Script.js
@@ -2,39 +2,48 @@
 // > MAIOR QUE
 // < MENOR QUE
 // != DIFERENTE (<>)
-// == IGUAL (= RECEBE, == COMPARE VALOR, === COMPARA VALOR E TIPO )
+// == IGUAL (= RECEBE, == COMPARA VALOR, === COMPARA VALOR E TIPO )
 // >= MAIOR OU IGUAL
 // <= MENOR OU IGUAL
 
-// OPERADORES ALTERNÀRIOS
-// && E (DUAS CONDIÇÔES DEVERÂO SER IGUAIS EM RESULTADO)
-// || OU (SE UMA DAS CONDIÇÒES FOR VERDADEIRA SERÀ EXECUTADO)
-// ! NÂO (SE NÃO FOR A CONDIÇÃO)
+// OPERADORES ALTERNÁRIOS
+// && E (DUAS CONDIÇÕES DEVERÃO SER IGUAIS EM RESULTADO)
+// || OU (SE UMA DAS CONDIÇÕES FOR VERDADEIRA SERÁ EXECUTADO)
+// ! NÃO (SE NÃO FOR A CONDIÇÃO)
 
 //CONDICIONAIS 
 // IF (SE SENAO)
 if(true){
     console.log("Sou verdadeiro");
 }else{
-    console.log("Sou falso")
+    console.log("Sou falso");
 }
 
-// verificar idade
+// verificar idade 
 var idade = Number(prompt("Qual é a sua idade?"));
 
 if(idade >= 18){
-   alert("Seja bem vindo");
+    alert("Seja bem vindo!");
 }else{
-
-if(idade == 0){
-    alert("Idade invalida , tente novamente!");
-    location.reload();
-}else{     
-     alert("Voce nao tem autorizaçao pra entrar no site");
-     location.href = "https://www.google.com.br/";
-     }
-     // location.reload();
+    if(idade == 0){
+        alert("Idade invalida, tente novamente!");
+        location.reload();
+    }else{
+        alert("Você não tem autorização para entrar nesse site!");
+        location.href = "https://www.google.com.br/?hl=pt-BR";
+    }
+   
+    // location.reload();
 }
 
-// condição ? bloco : bloco2 if ternario
-// x > 10 ? 
+// condição ? bloco1 : bloco2 if ternario
+// x > 10 ? alert("legal") : ("maneiro")
+
+
+// if (x > 0 && x < 10){
+// if (x > 0 || x < 10){
+// if (x != 10){
+//     alert("Sou maior que zero e menor que 10")
+// }else{
+//     alert("Sou menor que zero ou maior que 10")
+// }
