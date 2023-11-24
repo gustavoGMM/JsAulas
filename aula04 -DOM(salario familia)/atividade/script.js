17cb3233cac43f0bbc376d4d4f21cc997b9e9e0f
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-24 11:48:35 -0300

    update

--- aula04 -DOM(salario familia)/atividade/script.js
+++ aula04 -DOM(salario familia)/atividade/script.js
@@ -1,25 +1,23 @@
-// var empregado = document.getElementById("empregado");
-var empregado = document.querySelector("#empregado").value;
-var filhos = document.querySelector("#filhos");
-var salario = document.querySelector("#salario");
-var btnCalcular = document.querySelector("#bntCalcular");
+var btnCalcular = document.querySelector("#btnCalcular");
 
-btnCalcular.addEventListener("click",calcular );
-  
+btnCalcular.addEventListener("click", calcular );
+
 function calcular(){
     // var empregado = document.getElementById("empregado");
     let empregado = document.querySelector("#empregado").value;
     let filhos = document.querySelector("#filhos").value;
     let salario = document.querySelector("#salario").value;
-    let resultadoSpan = document.querySelector("#resultado").value;
-
-    if(filhos == 0 || salario == 0 || empregado == ''){ 
-        resultadoSpan.innerHTML = `Dados invalidos! Insira as informaçoes corretamente!` ;
-     else if(s)
-    if(salario <= 806.80){
+    let resultadoSpan = document.querySelector("#resultado");
+    
+    if(filhos == 0 || salario == 0 || empregado == ''){
+        resultadoSpan.innerHTML = `Dados inválidos! Insira as informações corretamente!`;
+    } else if(salario <= 806.80){
         let resultado = filhos * 41.37;
-        resultado.innerHTML = `O salario familia do empregado ${empregado} é de R$ ${resultado}`;
-        else if(salario >= 806.81 && salario <= 1212.64){
-            let resultado = filhos *29
+        resultadoSpan.innerHTML = `O salário família do empregado ${empregado} é de R$ ${resultado}`;
+    } else if(salario >= 806.81 && salario <= 1212.64){
+        let resultado = filhos * 29.16;
+        resultadoSpan.innerHTML = `O salário família do empregado ${empregado} é de R$ ${resultado}`;
+    } else{
+        resultadoSpan.innerHTML = `Não há salário família para o empregado ${empregado}`;
     }
 }
