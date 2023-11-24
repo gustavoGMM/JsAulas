652117f42459c9960856f3cfbbb6c12154567421
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-24 09:03:03 -0300

    update

---
+++ aula03 - repeticoes e array/correçao/script.js
@@ -0,0 +1,12 @@
+var filmes = ["Vingadores","Frozen","shrek","GodZilla","Senhor dos aneis","Harry Potter"]
+var Vingadores = ["Titulo: Vingadores","Frozen","shrek","GodZilla","Senhor dos aneis","Harry Potter"]
+
+//for (Var contador = 0; contador <= filmes.length; contador++)
+//     console.log(filmes[contador]);
+// }
+
+ filmes.forEach((melancia) => {
+ console.log(cadaFilme)
+ });
+
+ 
---
+++ aula03 - repeticoes e array/correçao/script3.js
@@ -0,0 +1,7 @@
+for (var contTabuada = 1; cont <= 10; cont++)
+      for (var cont = 1; cont <= 10; cont++){
+           console.log(`${cont} x ${contTabuada} = ${cont * contTabuada}`);
+        
+}
+
+console.log("---------------");
