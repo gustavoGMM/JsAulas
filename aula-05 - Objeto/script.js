b498764e31aec3971ac86f2dee3f538c24e32f69
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-27 10:12:50 -0300

    update

---
+++ aula-05 - Objeto/script.js
@@ -0,0 +1,28 @@
+//var cliente1Nome = "Leonardo";
+//var cliente1Saldo = 0;
+//var cliente1Cpf = 99999;
+//var cliente1Agencia = "001";
+//var cliente1Conta = "25123213";
+//var cliente1dataDeNascimento= "25/02/1998";
+
+//var cliente1 =["Nome = Leonardo", "Saldo = 99999"];
+
+class Cliente{
+    nome;
+    dataDeNascimento;
+    cfp;
+    conta;
+    agencia;
+    saldo;
+    sacar();
+   // secar();
+   // depositar();
+}
+
+var cliente1 = new Cliente();
+cliente1.nome = "Leonardo";
+cliente1.dataDeNascimento = "25/02/1998";
+cliente1.cfp = 99999;
+cliente1.conta = "001";
+cliente1.agencia = "x001";
+cliente1.saldo = 100;
