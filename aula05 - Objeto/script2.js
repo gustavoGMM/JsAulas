07e86a6f2905ffcc0023c1b2207428a68b6b6343
Gustavo Maia <gustavomaia855@gmail.com>
2023-11-27 11:19:41 -0300

    update

--- aula05 - Objeto/script2.js
+++ aula05 - Objeto/script2.js
@@ -1,12 +1,23 @@
 class Aluno{
-    constructor(valorNome,valorIdade){
+    constructor(valorNome,valorIdade,valorTurma){
         this.nome = valorNome;
         this.idade = valorIdade;
-        this.turma = "SENAI FRON";
+        this.turma = valorTurma;
     }
    
 }
 
-var aluno1 = new Aluno("Leandro", 25);
-var aluno2 = new Aluno("Alice", 20);
+class Curso{
+    constructor(valorNome,valorDuraçao,ValorTipo){
+        this.nome = valorNome;
+        this.duraçao = valorDuraçao;
+        this.turma = ValorTipo;
+    }
+   
+}
+var curso1 = new Curso("FRONT",220,"Tecnologia");
+var curso2 = new Curso("Java",400,"Tecnologia");
+
+var aluno1 = new Aluno("Leandro", 25, curso1);
+var aluno2 = new Aluno("Alice", 20, curso2);
 
