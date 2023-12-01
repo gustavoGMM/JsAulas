c058b582110f9434404a805361f4dee39f1d7516
Gustavo Maia <gustavomaia855@gmail.com>
2023-12-01 08:22:25 -0300

    update

--- aula07 e 08 - ECMASCRIPT/materialDeAula/script.js
+++ aula07 e 08 - ECMASCRIPT/materialDeAula/script.js
@@ -5,47 +5,48 @@ console.log(frase);
 console.log(frase.replace("mundo","bacana"));
 console.log(frase.replaceAll("mundo","bacana"));
 
-//SETATTRIBUTE
+// SETATTRIBUTE
 document.querySelector("span").setAttribute("style","color: blue ;");
 
 //CLASS LIST
-//document.querySelector("#classlist").classList.remove("claro");
-//document.querySelector("#classlist").classList.add("claro");
+document.querySelector("#classlist").classList.remove("claro");
+document.querySelector("#classlist").classList.add("claro");
 var elemento = document.querySelector("#classlist");
+// contains se contem a classe
+// toggle troca de classe
 if(elemento.classList.contains("claro")){
-  //  elemento.classList.toggle("escuro");
-    elemento.classList.replace("escuro","claro");
+    // elemento.classList.toggle("escuro");
+    elemento.classList.replace("claro","escuro");
 }
 
-//inde of /includes
+//index of / includes
 var frutas = ['Morango','Banana','Laranja','Uva']
 
 // includes -> true e false
 console.log(frutas.includes("Abacaxi"));
-//indexOf -> o valor indice do array (caso nao exista, -1)
+// indexOf -> o valor indice do array (caso não exista, -1)
 console.log(frutas.indexOf("Abacaxi"));
 
 if(frutas.includes("Abacaxi" == false)){
-    console.log("nao achei")
+    console.log("Não achei")
 }
 
 //SET TIMEOUT
 function msg(){
-    return new Promise((teste) =>{ 
-        setTimeout(() =>{ 
-    console.log("Bom dia");
-    resolve()
-},2000);
-
-})
-
+    //PROMISE - SE ASSEMELHA A UMA FUNÇÃO, PORÉM ELE CONFIGURA SUCESSO E ERRO
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            console.log("Bom dia!");
+            resolve()
+        }, 2000);
+        
+    })
 }
 
-async function carregando(){ 
-   console.log("iniciou");
-   await msg();
-   console.log("Seja bem vindo");
-
+async function carregando(){
+    console.log("iniciou");
+    await msg();
+    console.log("Seja bem vindo");
 }
 
-carregando ();
+carregando();
