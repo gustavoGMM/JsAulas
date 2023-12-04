54282b73c47cc69516d5a35191168f89b9093af2
Gustavo Maia <gustavomaia855@gmail.com>
2023-12-04 10:49:43 -0300

    update

---
+++ java/script.js
@@ -0,0 +1,6 @@
+// INSTALAÇAO
+// VISITE https://jquery.com/download/
+// clicar na opção "Download the uncompressed, development jQuery 3.7.1"
+// aberto o arquivo, aperte com o botão direito e salvar página como.. e salve em um local de sua preferencia!
+var titulo = $("#tituloDaPagina");
+console.log(titulo);
