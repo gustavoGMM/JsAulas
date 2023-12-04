ce43421f3e49e7f07cd8d21bd4573c5f7ce7f31b
Gustavo Maia <gustavomaia855@gmail.com>
2023-12-04 11:52:13 -0300

    update

--- java/script.js
+++ java/script.js
@@ -2,5 +2,34 @@
 // VISITE https://jquery.com/download/
 // clicar na opção "Download the uncompressed, development jQuery 3.7.1"
 // aberto o arquivo, aperte com o botão direito e salvar página como.. e salve em um local de sua preferencia!
-var titulo = $("#tituloDaPagina");
+
+//SELETOR JQUERY = $(# OU . ELEMENTO)
+var titulo = $("#tituloDaPagina").text("jquery aula");
 console.log(titulo);
+
+// var paragrafo = document.querySelector("#textoDaPagina");
+// function consoleListar(){
+//     console.log("Este é o texto do site");
+// }
+
+var paragrafo = $("textDaPagina").on("mouseover", function() {
+    console.log("Este é o texto do site");
+});
+
+//Comportamento
+// jquery tem capacidade de avaliar valores não definidos e vazios
+var usuario = "Victor";
+var usuarioIndefinido = undefined;
+
+var usuarioResultado = $("#testeIndefinido").text(usuarioIndefinido);
+
+//algotitmo contagem de caracteres
+var blocoDetexto = $("#blocoDeTexto").on("input", function(){;
+    let blocoDeTextoValue = $("#blocoDeTexto").val();
+    let resposta = $("numCaracteres")
+
+    var contagem = blocoDeTextoValue.length;
+    resposta.text(contagem);
+});
+    
+
